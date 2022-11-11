d3198213cf75cb23a90806ee7573b60523fa4643
ubermanu <manu@paca.happy-dev.fr>
2022-11-11 17:10:01 +0100

    add normal and insert modes

--- App.js
+++ App.js
@@ -7,36 +7,67 @@
  */
 
 import type { Node } from 'react';
-import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, View } from 'react-native';
+import React, { useState } from 'react';
+
+import { SafeAreaView, ScrollView, StatusBar, View, Text } from 'react-native';
 
 import CodeEditor, {
   CodeEditorSyntaxStyles,
 } from '@rivascva/react-native-code-editor';
+
+import TransparentButton from '@wniemiec-component-reactnative/transparent-button';
 
 const App: () => Node = () => {
   // TODO: Set the status bar styles according to the theme
   // TODO: Set a dark/light theme according to user preferences
   const editorStyles = CodeEditorSyntaxStyles.atomOneDark;
 
-  const backgroundStyle = {
+  const areaStyle = {
+    color: editorStyles.hljs.color,
     backgroundColor: editorStyles.hljs.background,
+    flex: 1,
   };
 
+  // The current mode of the editor (can be normal or insert)
+  const [mode, setMode] = useState('normal');
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={areaStyle}>
       <StatusBar
         barStyle={'light-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+        backgroundColor={areaStyle.backgroundColor}
       />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          padding: 10,
+          alignItems: 'center',
+        }}>
+        <View
+          style={{
+            flex: 1,
+          }}>
+          <Text style={{ color: areaStyle.color }}>main.js</Text>
+        </View>
+        <TransparentButton
+          title={mode.toUpperCase()}
+          onPress={() => setMode(mode === 'normal' ? 'insert' : 'normal')}
+          style={{ backgroundColor: '#00000000' }}
+          fgColor={areaStyle.color}
+        />
+      </View>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
           <CodeEditor
             style={{
-              fontSize: 20,
-              inputLineHeight: 26,
-              highlighterLineHeight: 26,
+              fontSize: 18,
+              inputLineHeight: 28,
+              highlighterLineHeight: 28,
             }}
+            readOnly={mode === 'normal'}
+            autoFocus={true}
+            initialValue={`function sum(a, b) {\n  return a + b;\n}`}
             language="javascript"
             syntaxStyle={CodeEditorSyntaxStyles.atomOneDark}
             showLineNumbers
